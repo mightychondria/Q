f0fc2463fc2df15fdb5d1036bad545990d49917f
Harun Davood <davoodharun@gmail.com>
2016-01-07 11:14:29 -0800

    working search and audio player

--- client/app/playlist/playlist.js
+++ client/app/playlist/playlist.js
@@ -4,22 +4,29 @@ angular.module('Q.playlist', [
 'ngRoute'
 ])
 
-.controller('playlistController', function($scope, $location, Playlist) {
-  $scope.songs= [];
-  Playlist.searchSongs().then(function(tracks){
-    console.log(tracks);
-    for(var i = 0;i<tracks.length;i++){
-      var track = {
-                        id: tracks[i].id,
-                        title: tracks[i].title,
-                        artist: tracks[i].genre,
-                        url: tracks[i].stream_url + "?client_id=f270bdc572dc8380259d38d8015bdbe7"
-                    };
-      $scope.$apply(function(){
-        $scope.songs.push(track);
-      })
-    }
-  })
+.controller('playlistController', function($scope, $rootScope, $location, Playlist) {
+ $rootScope.songs= [];
+
+  $scope.searchSong = function (){
+    $rootScope.songs= [];
+    return Playlist.searchSongs($scope.query).then(function(tracks){
+      console.log(tracks);
+      for(var i = 0;i<tracks.length;i++){
+        var track = {
+                          id: tracks[i].id,
+                          title: tracks[i].title,
+                          artist: tracks[i].genre,
+                          url: tracks[i].stream_url + "?client_id=f270bdc572dc8380259d38d8015bdbe7"
+                      };
+        $rootScope.$apply(function(){
+          $rootScope.songs.push(track);
+        })
+      }
+    })
+
+  }
+
+
         // SC.initialize({
         //     client_id: "f270bdc572dc8380259d38d8015bdbe7"
         // });
