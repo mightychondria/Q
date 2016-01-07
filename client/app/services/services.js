0cf09c667cfbe79c7ffde7f6768b986f93a109f7
Harun Davood <davoodharun@gmail.com>
2016-01-07 11:14:29 -0800

    working search and audio player

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -21,14 +21,14 @@ angular.module('Q.services', [
 
   }
 
-  var searchSongs = function(){
+  var searchSongs = function(query){
     SC.initialize({
       client_id: 'f270bdc572dc8380259d38d8015bdbe7'
     });
 
 
     return SC.get('/tracks', {
-      q: 'wrecking ball',
+      q: query,
     }).then(function(tracks) {
       return tracks;
     });
