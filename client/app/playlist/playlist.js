59243bbb47670575d728076ba79dde0026f19ee5
Harun Davood <davoodharun@gmail.com>
2016-01-06 21:50:20 -0800

    audio search from soundcloud

--- client/app/playlist/playlist.js
+++ client/app/playlist/playlist.js
@@ -6,29 +6,42 @@ angular.module('Q.playlist', [
 
 .controller('playlistController', function($scope, $location, Playlist) {
   $scope.songs= [];
-
-        SC.initialize({
-            client_id: "f270bdc572dc8380259d38d8015bdbe7"
-        });
-
-        SC.get("/groups/55517/tracks", {
-            limit: 5
-        }, function(tracks) {
-            for (var i = 0; i < tracks.length; i ++) {
-                SC.stream( '/tracks/' + tracks[i].id, function( sm_object ){
-                    var track = {
+  Playlist.searchSongs().then(function(tracks){
+    console.log(tracks);
+    for(var i = 0;i<tracks.length;i++){
+      var track = {
                         id: tracks[i].id,
                         title: tracks[i].title,
                         artist: tracks[i].genre,
-                        url: sm_object.url
+                        url: tracks[i].stream_url + "?client_id=f270bdc572dc8380259d38d8015bdbe7"
                     };
+      $scope.$apply(function(){
+        $scope.songs.push(track);
+      })
+    }
+  })
+        // SC.initialize({
+        //     client_id: "f270bdc572dc8380259d38d8015bdbe7"
+        // });
 
-                    $scope.$apply(function () {
-                        $scope.songs.push(track);
-                    });
-                });
-            }
-        });
+        // SC.get("/groups/55517/tracks", {
+        //     limit: 5
+        // }, function(tracks) {
+        //     for (var i = 0; i < tracks.length; i ++) {
+        //         SC.stream( '/tracks/' + tracks[i].id, function( sm_object ){
+        //             var track = {
+        //                 id: tracks[i].id,
+        //                 title: tracks[i].title,
+        //                 artist: tracks[i].genre,
+        //                 url: sm_object.url
+        //             };
+
+        //             $scope.$apply(function () {
+        //                 $scope.songs.push(track);
+        //             });
+        //         });
+        //     }
+        // });
   // Playlist.getSongs().then(function(songs){
 
   // })
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -21,13 +21,41 @@ angular.module('Q.services', [
 
   }
 
-  var searchSongs = function(query){
+  var searchSongs = function(){
+    SC.initialize({
+      client_id: 'f270bdc572dc8380259d38d8015bdbe7'
+    });
 
-  }
 
+    return SC.get('/tracks', {
+      q: 'wrecking ball',
+    }).then(function(tracks) {
+      return tracks;
+    });
+    // SC.get("/groups/55517/tracks", {
+    //         limit: 5
+    //     }, function(tracks) {
+    //         for (var i = 0; i < tracks.length; i ++) {
+    //             SC.stream( '/tracks/' + tracks[i].id, function( sm_object ){
+    //                 var track = {
+    //                     id: tracks[i].id,
+    //                     title: tracks[i].title,
+    //                     artist: tracks[i].genre,
+    //                     url: sm_object.url
+    //                 };
+
+
+    //                     tracks.push(track);
+
+    //             });
+    //         }
+    //     });
+    // return tracks;
+}
   return {
     getSongs: getSongs,
-    addSong: addSong
+    addSong: addSong,
+    searchSongs: searchSongs
   }
 
 })
