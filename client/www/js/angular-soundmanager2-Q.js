34081e0469b9f12746a48351cdc205e13f380e5e
JC <jc@jcmbp.local>
2016-01-10 19:26:23 -0800

    attempted to fix deployment

--- client/www/js/angular-soundmanager2-Q.js
+++ client/www/js/angular-soundmanager2-Q.js
@@ -4426,7 +4426,7 @@ ngSoundManager.factory('angularPlayer', ['$rootScope', '$log',
             volume = 90,
             trackProgress = 0,
             playlist = [],
-            socket = io('https://floating-earth-3947.herokuapp.com/');
+            socket = io('http://localhost:8000');
         
         socket.on('getQueue', function(queue){
             playlist = queue;
